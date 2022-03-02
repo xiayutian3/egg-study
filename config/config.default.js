13de7fa1baf896693e9f15a3d1ae0d5cd0b2ba34
1127071993@qq.com <aini2009626>
2022-03-03 00:24:30 +0800

    20.Egg.js配置连接MySql数据库

--- config/config.default.js
+++ config/config.default.js
@@ -54,6 +54,28 @@ module.exports = appInfo => {
   // // 全局挂载我们的中间件 counter
   // config.middleware = [ 'counter' ];
 
+
+  // 配置mysql数据库
+  config.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: '127.0.0.1',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: 'root',
+      // 数据库名
+      database: 'egg_article',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
--- config/plugin.js
+++ config/plugin.js
@@ -8,7 +8,14 @@
 //   // }
 // };
 
+// 开启ejs
 exports.ejs = {
   enable: true,
   package: 'egg-view-ejs',
+};
+
+// 开启mysql
+exports.mysql = {
+  enable: true,
+  package: 'egg-mysql',
 };
